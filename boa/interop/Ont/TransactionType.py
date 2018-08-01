a397de8517f7b4753645e74126a25be4179e88cc
zhoupw@gmail.com <zhoupw@gmail.com>
2018-08-01 11:54:14 +0800

    add ont syscall

---
+++ boa/interop/Ont/TransactionType.py
@@ -0,0 +1,43 @@
+
+def MinerTransaction():
+    pass
+
+
+def IssueTransaction():
+    pass
+
+
+def ClaimTransaction():
+    pass
+
+
+def EnrollmentTransaction():
+    pass
+
+
+def VotingTransaction():
+    pass
+
+
+def RegisterTransaction():
+    pass
+
+
+def ContractTransaction():
+    pass
+
+
+def StateTransaction():
+    pass
+
+
+def AgencyTransaction():
+    pass
+
+
+def PublishTransaction():
+    pass
+
+
+def InvocationTransaction():
+    pass
