2399d4e809556028e5bd4c940feaa58d3216222b
xris-hu <tcpdumpp@163.com>
2018-08-08 11:32:43 +0800

    modify system api

---
+++ boa/interop/System/Blockchain.py
@@ -0,0 +1,62 @@
+
+
+def GetHeight():
+    """
+
+    """
+    pass
+
+
+def GetHeader(height_or_hash):
+    """
+
+    :param height_or_hash:
+    """
+    pass
+
+
+def GetBlock(height_or_hash):
+    """
+
+    :param height_or_hash:
+    """
+    pass
+
+
+def GetTransaction(hash):
+    """
+
+    :param hash:
+    """
+    pass
+
+
+def GetAccount(script_hash):
+    """
+
+    :param script_hash:
+    """
+    pass
+
+
+def GetValidators():
+    """
+
+    """
+    pass
+
+
+def GetAsset(asset_id):
+    """
+
+    :param asset_id:
+    """
+    pass
+
+
+def GetContract(script_hash):
+    """
+
+    :param script_hash:
+    """
+    pass
