7c14071aa5c1a0e60b006c40c5573d369afc6eac
Daniel Liu <33597115+skyinglyh1@users.noreply.github.com>
2018-10-12 14:30:46 +0800

    Update OEP4Sample.py (#26)
    Correct typo error.

--- boa_test/example/OEP4Sample.py
+++ boa_test/example/OEP4Sample.py
@@ -4,37 +4,43 @@ An Example of OEP-4
 from boa.interop.System.Storage import GetContext, Get, Put, Delete
 from boa.interop.System.Runtime import Notify, CheckWitness
 from boa.builtins import concat, ToScriptHash
-
 ctx = GetContext()
 
-NAME = 'tokenName'
+NAME = 'TokenName'
 SYMBOL = 'Symbol'
 DECIMAL = 8
 FACTOR = 100000000
-# OWNER = ToScriptHash("AQf4Mzu1YJrhz9f3aRkkwSm9n3qhXGSh4p")
-OWNER = bytearray(b'\x61\x6f\x2a\x4a\x38\x39\x6f\xf2\x03\xea\x01\xe6\xc0\x70\xae\x42\x1b\xb8\xce\x2d')
+OWNER = ToScriptHash("AQf4Mzu1YJrhz9f3aRkkwSm9n3qhXGSh4p")
+# OWNER = bytearray(b'\x61\x6f\x2a\x4a\x38\x39\x6f\xf2\x03\xea\x01\xe6\xc0\x70\xae\x42\x1b\xb8\xce\x2d')
 TOTAL_AMOUNT = 1000000000
-TRANSFER_PREFIX = bytearray(b'\x01')
-APPROVE_PREFIX = bytearray(b'\x02 ')
+BALANCE_PREFIX = bytearray(b'\x01')
+APPROVE_PREFIX = bytearray(b'\x02')
 
-SUPPLY_KEY = 'totoalSupply'
+SUPPLY_KEY = 'totalSupply'
 
 
 def Main(operation, args):
     """
-
     :param operation:
     :param args:
     :return:
     """
-    if operation == 'name':
-        return Name()
-    if operation == 'totalSupply':
-        return TotalSupply()
+    # 'init' has to be invokded first after deploying the contract to store the necessary and important info in the blockchain
     if operation == 'init':
-        return Init()
+        return init()
+    if operation == 'name':
+        return name()
     if operation == 'symbol':
-        return Symbol()
+        return symbol()
+    if operation == 'decimals':
+        return decimals()
+    if operation == 'totalSupply':
+        return totalSupply()
+    if operation == 'balanceOf':
+        if len(args) != 1:
+            return False
+        acct = args[0]
+        return balanceOf(acct)
     if operation == 'transfer':
         if len(args) != 3:
             return False
@@ -42,16 +48,9 @@ def Main(operation, args):
             from_acct = args[0]
             to_acct = args[1]
             amount = args[2]
-            return Transfer(from_acct,to_acct,amount)
+            return transfer(from_acct,to_acct,amount)
     if operation == 'transferMulti':
-        return TransferMulti(args)
-    if operation == 'approve':
-        if len(args) != 3:
-            return False
-        owner  = args[0]
-        spender = args[1]
-        amount = args[2]
-        return Approve(owner,spender,amount)
+        return transferMulti(args)
     if operation == 'transferFrom':
         if len(args) != 4:
             return False
@@ -59,139 +58,187 @@ def Main(operation, args):
         from_acct = args[1]
         to_acct = args[2]
         amount = args[3]
-        return TransferFrom(spender,from_acct,to_acct,amount)
-    if operation == 'balanceOf':
-        if len(args) != 1:
+        return transferFrom(spender,from_acct,to_acct,amount)
+    if operation == 'approve':
+        if len(args) != 3:
             return False
-        acct = args[0]
-        return BalanceOf(acct)
-    if operation == 'decimal':
-        return Decimal()
+        owner  = args[0]
+        spender = args[1]
+        amount = args[2]
+        return approve(owner,spender,amount)
     if operation == 'allowance':
         if len(args) != 2:
             return False;
         owner = args[0]
         spender = args[1]
-        return Allowance(owner,spender)
+        return allowance(owner,spender)
 
 
-def Name():
-    return 'TokenName'
-
-
-def TotalSupply():
-    return TOTAL_AMOUNT * FACTOR
-
-
-def Init():
+def init():
+    """
+    initialize the contract, put some important info into the storage in the blockchain
+    :return:
+    """
     if Get(ctx,SUPPLY_KEY):
         Notify('Already initialized!')
         return False
     else:
         total = TOTAL_AMOUNT * FACTOR
         Put(ctx,SUPPLY_KEY,total)
-        Put(ctx,concat(TRANSFER_PREFIX,OWNER),total)
+        Put(ctx,concat(BALANCE_PREFIX,OWNER),total)
         Notify(['transfer', '', OWNER, total])
         return True
 
+def name():
+    """
+    :return: name of the token
+    """
+    return NAME
 
-def Symbol():
+
+def symbol():
+    """
+    :return: symbol of the token
+    """
     return SYMBOL
 
 
-def Transfer(from_acct,to_acct,amount):
-
-    if from_acct == to_acct:
-        return True
-    if amount == 0:
-        return True
-    if amount < 0 :
-        return False
-    if  CheckWitness(from_acct) == False:
-        return False
-    if len(to_acct) != 20:
-        return False
-    fromKey = concat(TRANSFER_PREFIX,from_acct)
-    fromBalance = Get(ctx,fromKey)
-    if fromBalance < amount:
-        return False
-    if fromBalance == amount:
-        Delete(ctx,fromKey)
-    else:
-        Put(ctx,fromKey,fromBalance - amount)
-
-    tokey = concat(TRANSFER_PREFIX,to_acct)
-    toBalance = Get(ctx,tokey)
-
-    Put(ctx,tokey,toBalance + amount)
-    Notify(['transfer',from_acct,to_acct,amount])
-    return True
-
-
-def TransferMulti(args):
-    for p in (args):
-        if len(p) != 3:
-            return False
-        if Transfer(p[0],p[1],p[2]) == False:
-            # return False #  wrong since the previous transaction will be successful
-            raise Exception("TransferMulti failed.")
-    return True
-
-
-def Approve(owner,spender,amount):
-    if amount < 0 :
-        return False
-    if CheckWitness(owner) == False:
-        return False
-    if len(spender) != 20:
-        return False
-    key = concat(concat(APPROVE_PREFIX,owner),spender)
-    Put(ctx, key, amount)
-    Notify(['approve', owner, spender, amount])
-    return True
-
-
-def TransferFrom(spender,from_acct,to_acct,amount):
-    if amount < 0 :
-        return False
-    if CheckWitness(spender) == False:
-        return False
-    if len(to_acct) != 20:
-        return False
-    appoveKey = concat(concat(APPROVE_PREFIX,from_acct),spender)
-    approvedAmount = Get(ctx,appoveKey)
-    if approvedAmount < amount:
-        return False
-    if approvedAmount == amount:
-        Delete(ctx,appoveKey)
-    else:
-        Put(ctx,appoveKey,approvedAmount - amount)
-    
-    fromKey = concat(TRANSFER_PREFIX,from_acct)
-    fromBalance = Get(ctx,fromKey)
-    if fromBalance < amount:
-        return False
-    if fromBalance == amount:
-        Delete(ctx,fromKey)
-    else:
-        Put(ctx,fromKey,fromBalance - amount)
-
-    tokey = concat(TRANSFER_PREFIX,to_acct)
-    toBalance = Get(ctx,tokey)
-
-    Put(ctx,tokey,toBalance + amount)
-    Notify(['transfer',from_acct,to_acct,amount])
-    return True
-
-
-def BalanceOf(account):
-    return Get(ctx,concat(TRANSFER_PREFIX,account))
-
-
-def Decimal():
+def decimals():
+    """
+    :return: the decimals of the token
+    """
     return DECIMAL
 
 
-def Allowance(owner,spender):
+def totalSupply():
+    """
+    :return: the total supply of the token
+    """
+    return Get(ctx, SUPPLY_KEY)
+
+
+def balanceOf(account):
+    """
+    :param account:
+    :return: the token balance of account
+    """
+    return Get(ctx,concat(BALANCE_PREFIX,account))
+
+
+def transfer(from_acct,to_acct,amount):
+    """
+    Transfer amount of tokens from from_acct to to_acct
+    :param from_acct: the account from which the amount of tokens will be transferred
+    :param to_acct: the account to which the amount of tokens will be transferred
+    :param amount: the amount of the tokens to be transferred
+    :return: True means success, False or raising exception means failure.
+    """
+    if from_acct == to_acct or amount == 0:
+        return True
+    if CheckWitness(from_acct) == False:
+        return False
+    if len(to_acct) != 20 or len(from_acct) != 20:
+        return False
+    fromKey = concat(BALANCE_PREFIX,from_acct)
+    fromBalance = Get(ctx,fromKey)
+    if amount > fromBalance:
+        return False
+    if amount == fromBalance:
+        Delete(ctx,fromKey)
+    else:
+        Put(ctx,fromKey,fromBalance - amount)
+
+    toKey = concat(BALANCE_PREFIX,to_acct)
+    toBalance = Get(ctx,toKey)
+    Put(ctx,toKey,toBalance + amount)
+
+    Notify(['transfer',from_acct,to_acct,amount])
+
+    return True
+
+
+def transferMulti(args):
+    """
+    :param args: the parameter is an array, containing element like [from, to, amount]
+    :return: True means success, False or raising exception means failure.
+    """
+    for p in args:
+        if len(p) != 3:
+            # return False
+            raise Exception("transferMulti params error.")
+        if transfer(p[0], p[1], p[2]) == False:
+            # return False # this is wrong since the previous transaction will be successful
+            raise Exception("transferMulti failed.")
+    return True
+
+
+def approve(owner,spender,amount):
+    """
+    owner allow spender to spend amount of token from owner account
+    Note here, the amount should be less than the balance of owner right now.
+    :param owner:
+    :param spender:
+    :param amount:
+    :return: True means success, False or raising exception means failure.
+    """
+    if amount > balanceOf(owner):
+        return False
+    if CheckWitness(owner) == False:
+        return False
+    if len(spender) != 20 or len(owner) != 20:
+        raise Exception("approve params failed")
+    key = concat(concat(APPROVE_PREFIX,owner),spender)
+    Put(ctx, key, amount)
+    Notify(['approval', owner, spender, amount])
+    return True
+
+
+def transferFrom(spender,from_acct,to_acct,amount):
+    """
+    spender spend amount of token on the behalf of from_acct, spender makes a transaction of amount of tokens
+    from from_acct to to_acct
+    :param spender:
+    :param from_acct:
+    :param to_acct:
+    :param amount:
+    :return:
+    """
+    if CheckWitness(spender) == False:
+        return False
+    if len(spender) != 20 or len(from_acct) != 20 or len(to_acct) != 20:
+        raise Exception("transferFrom params failed")
+
+    fromKey = concat(BALANCE_PREFIX, from_acct)
+    fromBalance = Get(ctx, fromKey)
+    if amount > fromBalance:
+        return False
+
+    approveKey = concat(concat(APPROVE_PREFIX,from_acct),spender)
+    approvedAmount = Get(ctx,approveKey)
+    toKey = concat(BALANCE_PREFIX,to_acct)
+    toBalance = Get(ctx, toKey)
+    if amount > approvedAmount:
+        return False
+    elif amount == approvedAmount:
+        Delete(ctx,approveKey)
+        Delete(ctx, fromKey)
+    else:
+        Put(ctx,approveKey,approvedAmount - amount)
+        Put(ctx, fromKey, fromBalance - amount)
+
+    Put(ctx, toKey, toBalance + amount)
+
+    Notify(['transfer',spender, from_acct,to_acct,amount])
+
+    return True
+
+
+def allowance(owner,spender):
+    """
+    check how many token the spender is allowed to spend from owner account
+    :param owner: token owner
+    :param spender:  token spender
+    :return: the allowed amount of tokens
+    """
     allowanceKey = concat(concat(APPROVE_PREFIX,owner),spender)
     return Get(ctx,allowanceKey)
